6d54973d9aa25b07b3e01b600ffacdbd7aee6476
lewis <liuguoqing@yongche.com>
2019-06-20 08:44:01 +0800

    perfect a_list.cpp

--- src/a_list.cpp
+++ src/a_list.cpp
@@ -27,25 +27,38 @@ class AList: public List<Elem> {
 
         // 设置list中位置
         bool setPos(int pos) {
-            if ( (pos >= 0) && (pos <= listSize) ) {
+            if ((pos >= 0) && (pos <= listSize)) {
                 fence = pos;
             } 
             return ( pos >= 0 ) && ( pos <= listSize );
         }
 
         // 插入一个元素
-        bool insert(const Elem& e) {
-
+        bool insert(const Elem& item) {
+            if (listSize == maxSize) return false;
+            // 移动插入位置之后元素，让出插入空间
+            for (int i = listSize; i > fence; i--){
+                listArray[i] = listArray[i-1];
+            }
+            listArray[fence] = item;
+            listSize++; // list 中数量自增 1
+            return true;
         }
 
         // 删除一个元素
-        bool remove(Elem &e) {
-
+        bool remove(Elem &it) {
+            if (listSize == fence) return false;
+            it = listArray[fence];
+            for (int i = fence; i < listSize - 1; i++){
+                listArray[i] = listArray[i+1];
+            }
+            listSize--;
+            return true;
         }
         
         // 获取一个元素
         bool getValue(Elem& it) {
-            if (fence == listSize ) {
+            if (fence == listSize) {
                 return false;
             } else {
                 it = listArray[fence];
